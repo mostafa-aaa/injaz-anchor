6105fbe853d41282a199d3caaa414951c78f493b
Mostafa Abbas <mostafa.swcr@gmail.com>
2021-02-06 18:49:05 +0300

    create session entity

---
+++ src/InjazAnchor.Data/Entities/Sessions/Session.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace InjazAnchor.Data.Entities.Sessions
+{
+    public class Session : BaseEntity
+    {
+        public DateTime StartedOn { get; set; }
+
+        public DateTime EndedOn { get; set; }
+    }
+}
